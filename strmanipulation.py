05013be72db00b5856e07e24eac171506dcc7c64
arrrtsiomm <artemijzagzeckij@gmail.com>
2026-07-02 21:34:45 +0300

    doing it in freecodecamp's terminal

--- strmanipulation.py
+++ strmanipulation.py
@@ -1,1 +1,12 @@
-a = 'lll'
+text = 'Hello world'
+#print(text) #just prints a variable
+#print(text[8]) prints a letter whis on position 8
+#print(len(text)) prints lenght of the string
+#print(type(text))
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index]
+print(shifted)
