e5b106c1e42777674954c036ebb05b47fcf3f1fe
arrrtsiomm <artemijzagzeckij@gmail.com>
2026-07-05 17:21:32 +0300

    д

--- strmanipulation.py
+++ strmanipulation.py
@@ -6,5 +6,10 @@ text = 'Hello world'
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-for char in text:
-    print(char)
+encrypted_text = ''
+
+for char in text.lower():
+    index = alphabet.find(char)    
+    new_index = index + shift
+    encrypted_text = alphabet[new_index]
+    print('char:', char, 'encrypted_text:', encrypted_text)
